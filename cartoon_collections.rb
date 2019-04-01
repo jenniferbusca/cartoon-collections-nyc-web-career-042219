66198c3bb5f24c46a2723f7926e447e63129c01a
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-01 17:22:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  new_arr = calls.map do | call |
+  new_arr = []
+  calls.collect do | call |
     new_arr.push(call[0].upcase + call[1..-1].downcase + "!")
   end
   return new_arr
