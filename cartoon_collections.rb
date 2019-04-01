4c04161d6eeb6369d7a33e9a3787baa084747ab1
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-01 17:37:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ end
 
 def long_planeteer_calls(calls)
   calls.each do |call|
-    if call.length <= 4
-      return false
+    if call.length > 4
+      return TRUE
     else
-      return true
+      return false
     end
   end
 end
