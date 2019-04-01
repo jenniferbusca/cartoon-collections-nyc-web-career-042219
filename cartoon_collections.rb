dd80b3ca9aff7fb654412b7c18803f1e4647d016
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-01 17:19:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
+  new_arr = []
   calls.collect do | call |
     new_arr.push(call.[0].upcase + call[1..-1].downcase,"!")
+  end
+  return new_arr
 end
 
 def long_planeteer_calls# code an argument here
